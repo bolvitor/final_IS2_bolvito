d2fdd49d96ce75a858908273baa3848775ba1981
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-08-05 09:01:47 -0600

    creacion del archivo index de empleados en views

--- views/layout.php
+++ views/layout.php
@@ -39,6 +39,9 @@
                             <li>
                                 <a class="dropdown-item nav-link text-white " href="/final_IS2_bolvito/areas"><i class="ms-lg-0 ms-2 bi bi-plus-circle me-2"></i>Areas</a>
                             </li>
+                            <li>
+                                <a class="dropdown-item nav-link text-white " href="/final_IS2_bolvito/empleados"><i class="ms-lg-0 ms-2 bi bi-plus-circle me-2"></i>Empleados</a>
+                            </li>
                         
                     
                         
