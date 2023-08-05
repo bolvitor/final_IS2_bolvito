e8eb184cf675378a53407016147fd106cd45fc1c
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-08-05 11:25:06 -0600

    correcciones en la funcion buscar y en la tabla de empleados

--- tablas.sql
+++ tablas.sql
@@ -4,10 +4,8 @@ CREATE TABLE empleados (
     empleado_dpi INTEGER NOT NULL,
     empleado_edad INTEGER NOT NULL,
     empleado_sexo VARCHAR(100) NOT NULL,
-    empleado_id_area INTEGER NOT NULL,
     empleado_situacion smallint not null default 1,
-    PRIMARY KEY(empleado_id),
-    FOREIGN KEY (empleado_id_area) REFERENCES areas(area_id)
+    PRIMARY KEY(empleado_id)
 );
 
 CREATE TABLE puestos (
@@ -25,11 +23,15 @@ CREATE TABLE areas (
     PRIMARY KEY(area_id)
 );
 
-CREATE TABLE asignacion_puestos (
+
+CREATE TABLE asignacion_Empleado (
     asignacion_id SERIAL NOT NULL,
     empleado_id INTEGER NOT NULL,
     puesto_id INTEGER NOT NULL,
+    area_id INTEGER NOT NULL,
     PRIMARY KEY(asignacion_id),
     FOREIGN KEY (empleado_id) REFERENCES empleados(empleado_id),
-    FOREIGN KEY (puesto_id) REFERENCES puestos(puesto_id)
+    FOREIGN KEY (puesto_id) REFERENCES puestos(puesto_id),
+    FOREIGN KEY (area_id) REFERENCES areas(area_id)
 );
+
